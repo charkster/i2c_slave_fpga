f534d1bf2d9da1005bb7e6c0220657e5a8b095bd
charkster <31560568+charkster@users.noreply.github.com>
2025-08-29 15:41:16 -0700

    Update i2c_slave_top.sv

--- fpga/i2c_slave_top.sv
+++ fpga/i2c_slave_top.sv
@@ -18,6 +18,8 @@ module i2c_slave_top (
   logic [1:0] sda_shift;
   
   assign sda_in = sda; // this is for read-ablity
+
+  logic rst_i2c_n; // optional to reset i2c_slave if sda is stuck low
   
   // button_0 is high when pressed
   synchronizer u_synchronous_rst_n
@@ -36,17 +38,17 @@ module i2c_slave_top (
   i2c_slave 
   # ( .SLAVE_ID(7'h24) )
   u_i2c_slave
-  ( .rst_n,                // input 
-    .scl,                  // input 
-    .sda_in,               // input 
-    .sda_out,              // output  
-    .i2c_active      ( ),  // output 
-    .rd_en           ( ),  // output
-    .wr_en           ( ),  // output
-    .rdata,                // input [7:0]
-    .addr,                 // output [7:0]
-    .wdata,                // output [7:0]
-    .wr_en_wdata           // output
+  ( .rst_n (rst_n & rst_i2c_n), // input 
+    .scl,                       // input 
+    .sda_in,                    // input 
+    .sda_out,                   // output  
+    .i2c_active          ( ),   // output 
+    .rd_en               ( ),   // output
+    .wr_en               ( ),   // output
+    .rdata,                     // input [7:0]
+    .addr,                      // output [7:0]
+    .wdata,                     // output [7:0]
+    .wr_en_wdata                // output
   );
   
   synchronizer u_wr_en_sync
@@ -69,4 +71,23 @@ module i2c_slave_top (
     .register_3 ( )           // output [7:0]
   );
 
+  ///// This logic is optional, to reset i2c_slave in case sda is stuck low
+  logic        sda_out_sync;
+  logic [15:0] inactive_count;
+
+  synchronizer u_sda_out_sync
+  ( .clk,                   // input
+   .rst_n    (rst_n),       // input
+   .data_in  (sda_out),     // input
+   .data_out (sda_out_sync) // output
+  );
+
+  always_ff @(posedge clk, negedge rst_n)
+    if (~rst_n)                    inactive_count <= 'd0;
+    else if (sda_out_sync == 1'b0) inactive_count <= inactive_count + 'd1;
+    else                           inactive_count <= 'd0
+
+  always_ff @(posedge clk, negedge rst_n)
+    if (~rst_n)                       rst_i2c_n <= 1'b1;
+  else if (inactive_count == 'd32000) rst_i2c_n <= 1'b0; // this value is dependent on the FPGA board clock, duration should be 1ms or a bit more
 endmodule
